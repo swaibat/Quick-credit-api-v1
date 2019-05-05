c3ad2cc4945a86b2b21ad212f76ca5a42b865722
Rumbiiha swaibu <rumbiihas@gmail.com>
2019-05-05 11:43:18 +0300

    Implement Signin Route

--- api/routes/users.mjs
+++ api/routes/users.mjs
@@ -1,6 +1,6 @@
 
 import express from 'express';
-import {inputValidator,checkUserExists,postData} from '../midleware/auth';
+import {inputValidator,checkUserExists,postData,postSignin} from '../midleware/auth';
 
 
 const router = express.Router();
@@ -9,5 +9,9 @@ const router = express.Router();
 // signup route
 router.post('/auth/signup',inputValidator,checkUserExists,postData);
 
+// signin route
+router.post('/auth/signin',postSignin);
+    
+  
 
 export default router;

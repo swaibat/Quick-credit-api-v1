a3f044c7d999b8e877ab5772862f9d0b51731841
rumbiiha swaibu <rumbiihas@gmail.com>
2019-05-17 04:09:27 +0300

    Add swagger documentation to api

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import express from 'express'
 import usersRoute from './api/routes/users'
 import loansRoute from './api/routes/loans'
+import swaggerUI from 'swagger-ui-express';
+import swaggerDoc from './swagger.json';
 
 const app = express();
 app.use(express.json());
 
+app.use('/doc', swaggerUI.serve, swaggerUI.setup(swaggerDoc));
 app.use('/api/v1/users', usersRoute);
 app.use('/api/v1/loans', loansRoute);
 

def6076dc1e2aa01ef7bdca7217da623a2bf9f79
rumbiiha swaibu <rumbiihas@gmail.com>
2019-05-21 19:43:28 +0200

    set up cenvironment config

--- api/controllers/usersController.js
+++ api/controllers/usersController.js
@@ -1,11 +1,12 @@
 import jwt from 'jsonwebtoken';
 import { users } from '../models/users';
-const appSecreteKey = 'hksuua7as77hjvb348b3j2hbrbsc9923k';
+import dotenv from 'dotenv'
+dotenv.config()
 
 export class User {
   postData(req, res) {
     // token const
-    const token = jwt.sign({ email: req.body.email }, appSecreteKey, { expiresIn: '1hr' });
+    const token = jwt.sign({ email: req.body.email }, process.env.database, { expiresIn: '1hr' });
     const user = {
       id: Math.random().toString(35).slice(2),
       token,
@@ -23,7 +24,7 @@ export class User {
   // sigin
   postSignin(req, res) {
     // token const
-    const token = jwt.sign({ email: req.body.email }, appSecreteKey, { expiresIn: '1hr' });
+    const token = jwt.sign({ email: req.body.email }, process.env.database, { expiresIn: '1hr' });
     // check for the details existance
     const user = users.find(u => u.email === req.body.email);
     if (!user || user.password !== req.body.password) {

ab147a383c9734ca0791b25ab42638f18165a19f
rumbiiha swaibu <rumbiihas@gmail.com>
2019-05-21 15:00:51 +0200

    Add loans tables to db file

--- api/config/db.js
+++ api/config/db.js
@@ -6,9 +6,7 @@ const config = {
   user: process.env.user, // this is the db user credential
   database:process.env.database,
   password: process.env.password,
-  port: process.env.port,
-  max: 10, // max number of clients in the pool
-  idleTimeoutMillis: 30000,
+  port: process.env.port
 };
 
 const pool = new pg.Pool(config);
@@ -38,6 +36,59 @@ const createTables = () => {
     .catch((err) => {
       return err;
     });
+
+    const createTables = () => {
+      const Users = `CREATE TABLE IF NOT EXISTS
+      users (
+        id SERIAL PRIMARY KEY,
+        firstName VARCHAR (128) NOT NULL,
+        lastName VARCHAR(128) NOT NULL,
+        address TEXT NOT NULL,
+        email VARCHAR (355) UNIQUE NOT NULL,
+        password VARCHAR(128) NOT NULL,
+        status VARCHAR(128) NOT NULL,
+        createdOn TIMESTAMP DEFAULT Now(),
+        modifiedOn TIMESTAMP NOT NULL,
+        isAdmin BOOLEAN NOT NULL
+       )`;
+      pool.query(Users).catch((err) => {
+        return err;
+        pool.end();
+      });
+
+      const Loan = `CREATE TABLE IF NOT EXISTS
+        loan (
+            id SERIAL PRIMARY KEY,
+            users VARCHAR (355) NOT NULL,
+            createdOn TIMESTAMP DEFAULT Now(),
+            status VARCHAR(128) NOT NULL,
+            repaid BOOLEAN NOT NULL,
+            tenor INT NOT NULL,
+            amount FLOAT(4) NOT NULL,
+            paymentInstallment FLOAT(4) NOT NULL,
+            balance FLOAT(4) NOT NULL,
+            interest FLOAT(4) NOT NULL
+        )`;
+      pool.query(Loan)
+        .catch((err) => {
+          return err;
+        });
+
+      const loanRepayment = `CREATE TABLE IF NOT EXISTS
+        loanRepayment (
+              id SERIAL PRIMARY KEY,
+              loanId SERIAL NOT NULL,
+              createdOn TIMESTAMP DEFAULT Now(),
+              amount FLOAT(4) NOT NULL,
+              monthlyInstallment FLOAT(4) NOT NULL,
+              paidAmount FLOAT(4) NOT NULL,
+              balance FLOAT(4) NOT NULL
+          )`;
+      pool.query(loanRepayment)
+        .catch((err) => {
+          return err;
+        });
+    };
 };
 
 pool.on('remove', () => {
@@ -52,4 +103,4 @@ export {
   pool,
 }
 
-require('make-runnable');
+require('make-runnable')

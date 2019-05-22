3e8b21e2779679bee79cabf8c3c61bc714222598
rumbiiha swaibu <rumbiihas@gmail.com>
2019-05-22 16:14:14 +0200

    add user sign up to database post

---
+++ api/config/db.js
@@ -0,0 +1,76 @@
+import pg from 'pg';
+// import dotenv from 'dotenv'
+// dotenv.config()
+
+const config = {
+  user: 'postgres', //this is the db user credential
+  database: 'quick_credit',
+  password: 'Kanyanyama01',
+  port: 5432,
+  max: 10, // max number of clients in the pool
+  idleTimeoutMillis: 30000,
+};
+
+const pool = new pg.Pool(config);
+
+pool.on('connect', () => {
+  console.log('connected to the Database');
+});
+
+const createTables = () => {
+
+  const users = `CREATE TABLE IF NOT EXISTS
+      users (
+        id SERIAL PRIMARY KEY,
+        firstName VARCHAR (50) NOT NULL,
+        lastName VARCHAR (50) NOT NULL,
+        email VARCHAR (50)  NOT NULL,
+        address VARCHAR (50) NOT NULL,
+        password VARCHAR(50) NOT NULL
+       )`;
+    pool.query(users)
+      .then((res) => {
+        console.log(res);
+        pool.end();
+      });
+      
+    const loans = `CREATE TABLE IF NOT EXISTS
+        loans(
+          id SERIAL PRIMARY KEY,
+          createdOn timestamp without time zone DEFAULT now() NOT NULL,
+          tenor INT NOT NULL,
+          amount INT NOT NULL,
+          repaid boolean DEFAULT false NOT NULL,
+          paymentInstallment INT NOT NULL,
+          balance FLOAT NOT NULL,
+          interest FLOAT NOT NULL
+        )`;
+    pool.query(loans)
+      .then((res) => {
+        console.log(res);
+        pool.end();
+      })
+      .catch((err) => {
+        console.log(err);
+        pool.end();
+      });
+
+      
+  }
+
+  pool.on('remove', () => {
+    // eslint-disable-next-line no-console
+    console.log('client removed');
+    process.exit(0);
+  });
+  
+
+
+//export pool and createTables to be accessible  from an where within the application
+module.exports = {
+  createTables,
+  pool,
+};
+
+require('make-runnable');
+
